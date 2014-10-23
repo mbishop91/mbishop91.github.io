f8d9b666472b37ed592e542f3e7eba298c47465c
Michael Bishop <mbishop012000@gmail.com>
2014-10-22 21:35:06 -0400

    colors will change right on body load

--- trelloColor/trelloColor.js
+++ trelloColor/trelloColor.js
@@ -10,15 +10,18 @@ pomegranate = '#cb4d4d',
 clouds = '#ecf0f1',
 carrot = '#e09952',
 peterRiver = '#4d77cb';
+
+function changeColors() {
+	$("h2:contains('To Do - Site')").css('color', clouds).parents('.list').css('background', amethyst);
+	$("h2:contains('Doing - Site')").css('color', clouds).parents('.list').css('background', amethyst);
+	$("h2:contains('To Do - App')").css('color', clouds).parents('.list').css('background', carrot);
+	$("h2:contains('Doing - App')").css('color', clouds).parents('.list').css('background', carrot);
+	$("h2:contains('To Do - API')").css('color', clouds).parents('.list').css('background', peterRiver);
+	$("h2:contains('Doing - API')").css('color', clouds).parents('.list').css('background', peterRiver);
+	$("h2:contains('known issues')").css('color', clouds).parents('.list').css('background', pomegranate);
+	$("h2:contains('Done')").css('color', clouds).parents('.list').css('background', emerald);
+}
+
 document.addEventListener("DOMContentLoaded", function(){
-	$('body').hover(function() {
-		$("h2:contains('To Do - Site')").css('color', clouds).parents('.list').css('background', amethyst);
-		$("h2:contains('Doing - Site')").css('color', clouds).parents('.list').css('background', amethyst);
-		$("h2:contains('To Do - App')").css('color', clouds).parents('.list').css('background', carrot);
-		$("h2:contains('Doing - App')").css('color', clouds).parents('.list').css('background', carrot);
-		$("h2:contains('To Do - API')").css('color', clouds).parents('.list').css('background', peterRiver);
-		$("h2:contains('Doing - API')").css('color', clouds).parents('.list').css('background', peterRiver);
-		$("h2:contains('known issues')").css('color', clouds).parents('.list').css('background', pomegranate);
-		$("h2:contains('Done')").css('color', clouds).parents('.list').css('background', emerald);
-	});
+	changeColors(); $('body').hover(function() {changeColors();});
 },false);
