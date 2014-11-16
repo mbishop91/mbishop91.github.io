ec402963f9f44424e3c38cf1bbfd9e4329155d14
Michael Bishop <mbishop012000@gmail.com>
2014-11-16 15:21:33 -0500

    added the tampermonkey script file

---
+++ trelloColor/tampermonkey.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Trello List-Color
+// @version      0.1
+// @description  This script will change the colors of lists on a trello board
+// @author       Michael Bishop
+// @match        https://trello.com/*
+// @require https://mbishop91.github.io/trelloColor/trelloColor.js
+// @grant        none
+// ==/UserScript==
